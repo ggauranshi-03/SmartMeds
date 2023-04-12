f869d83a15ce6a4aefef2bfc6cf2296ac933bbd9
Anant Jain <ritujainanant2810@gmail.com>
2023-04-12 14:29:47 +0530

    front-end setup initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e7c190e7f0e851b0"; }
+/******/ 		__webpack_require__.h = function() { return "8836c1705b8953f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- ethereum/record.js
+++ ethereum/record.js
@@ -9,21 +9,92 @@
 // export default instance;\
 
 
-const fs = require('fs');
-const path = require('path');
-// const contractPath = path.join(__dirname, 'path/to/your/contract.json');
-const contractPath = `../artifacts/contracts/Record.sol/Record.json`;
-const contractJSON = fs.readFileSync(contractPath, 'utf8');
-const contract = JSON.parse(contractJSON);
+// const fs = require('fs');
+// const path = require('path');
+// // const contractPath = path.join(__dirname, 'path/to/your/contract.json');
+// const contractPath = `../artifacts/contracts/Record.sol/Record.json`;
+// const contractJSON = fs.readFileSync(contractPath, 'utf8');
+// const contract = JSON.parse(contractJSON);
 
-if (!contract.interface) {
-  console.error('Error parsing contract ABI: interface is undefined');
+// if (!contract.interface) {
+//   console.error('Error parsing contract ABI: interface is undefined');
+// }
+
+// const contractABI = JSON.parse(contract.interface);
+// const contractAddress = '0x06f1611e64439a5a911e35aa8b5cb6b814400482'; // Set the address of your deployed contract here
+
+// const web3 = new Web3(provider);
+// const instance = new web3.eth.Contract(contractABI, contractAddress);
+
+// export default instance
+
+
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+// const util = require('util');
+// // const contractPath = path.join(__dirname, 'path/to/your/contract.json');
+// const contractPath = `../artifacts/contracts/Record.sol/Record.json`;
+
+// // Use promisify to convert fs.readFile to a promise-based function
+// const readFile = util.promisify(fs.readFile);
+
+// async function loadContract() {
+//   try {
+//     const contractJSON = await readFile(contractPath, 'utf8');
+//     const contract = JSON.parse(contractJSON);
+
+//     if (!contract.interface) {
+//       console.error('Error parsing contract ABI: interface is undefined');
+//     }
+
+//     const contractABI = JSON.parse(contract.interface);
+//     const contractAddress = '0x06f1611e64439a5a911e35aa8b5cb6b814400482'; // Set the address of your deployed contract here
+
+//     const web3 = new Web3(provider);
+//     const instance = new web3.eth.Contract(contractABI, contractAddress);
+
+//     return instance;
+//   } catch (error) {
+//     console.error(`Error loading contract: ${error}`);
+//   }
+// }
+
+// export default loadContract();
+
+
+
+
+
+const fs = require('fs').promises;
+const path = require('path');
+const util = require('util');
+
+// const contractPath = path.join(__dirname, '../artifacts/contracts/Record.sol/Record.json');
+const contractPath = `../artifacts/contracts/Record.sol/Record.json`;
+
+async function loadContract() {
+  try {
+    const contractJSON = await fs.readFile(contractPath, 'utf8');
+    const contract = JSON.parse(contractJSON);
+
+    if (!contract.interface) {
+      console.error('Error parsing contract ABI: interface is undefined');
+    }
+
+    const contractABI = JSON.parse(contract.interface);
+    const contractAddress = '0x06f1611e64439a5a911e35aa8b5cb6b814400482'; // Set the address of your deployed contract here
+
+    const web3 = new Web3(provider);
+    const instance = new web3.eth.Contract(contractABI, contractAddress);
+
+    return instance;
+  } catch (error) {
+    console.error(`Error loading contract: ${error}`);
+  }
 }
 
-const contractABI = JSON.parse(contract.interface);
-const contractAddress = '0x06f1611e64439a5a911e35aa8b5cb6b814400482'; // Set the address of your deployed contract here
+export default loadContract();
 
-const web3 = new Web3(provider);
-const instance = new web3.eth.Contract(contractABI, contractAddress);
-
-export default instance
